bc445d1d6d529dc7faddc159e35fb5726efa56fb
Drvita <chavagalindo@live.com.mx>
2021-01-05 00:20:48 -0600

    Add print exam 05012021

---
+++ public/precache-manifest.6b23d3404f7ced7fcf38fbd128e0c292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045829dc0a0eb9cb94791f4b683d0d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35533ce875492e67aa9",
+    "url": "/static/js/2.42b32271.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/static/js/2.42b32271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482bfe18570297d9c8db",
+    "url": "/static/js/main.c121b186.chunk.js"
+  },
+  {
+    "revision": "4ec92c0b69524f18563c",
+    "url": "/static/js/runtime-main.402a8ddb.js"
+  }
+]);
