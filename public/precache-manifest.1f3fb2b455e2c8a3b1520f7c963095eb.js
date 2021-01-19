7a519f133681d30e283709354a0bc4cea0542983
Drvita <chavagalindo@live.com.mx>
2021-01-19 15:11:09 -0600

    debug printExam 19012021

---
+++ public/precache-manifest.1f3fb2b455e2c8a3b1520f7c963095eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125886e5e90d6e42e2ce82bd5187a678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0d3b30dff5ebef494d",
+    "url": "/static/js/2.a6952c7b.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/static/js/2.a6952c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14341fc9a0b886e28ca4",
+    "url": "/static/js/main.48cc506d.chunk.js"
+  },
+  {
+    "revision": "4ec92c0b69524f18563c",
+    "url": "/static/js/runtime-main.402a8ddb.js"
+  }
+]);
