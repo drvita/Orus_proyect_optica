c5e8289aba7ca463e5faaad6f9a07c74e1affe1d
Drvita <chavagalindo@live.com.mx>
2021-02-15 13:35:36 -0600

    update methodpay 2

---
+++ public/precache-manifest.0108773b651276b5a82a0ea13579edc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6850969a354c4da8366ef1e87e9bb653",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0d3b30dff5ebef494d",
+    "url": "/static/js/2.a6952c7b.chunk.js"
+  },
+  {
+    "revision": "cf0565a1cf8a621bfb906afd263c3a1b",
+    "url": "/static/js/2.a6952c7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8845114160930c9af8be",
+    "url": "/static/js/main.2f37339a.chunk.js"
+  },
+  {
+    "revision": "4ec92c0b69524f18563c",
+    "url": "/static/js/runtime-main.402a8ddb.js"
+  }
+]);
