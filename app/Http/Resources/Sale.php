a46e59a01b3a2c3445e9a4e6c36a56994c198095
Salvador Galindo <chavagalindo@live.com.mx>
2021-08-18 14:20:23 -0500

    Upgrade sales compoenent

--- app/Http/Resources/Sale.php
+++ app/Http/Resources/Sale.php
@@ -6,18 +6,23 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class Sale extends JsonResource{
 
     public function toArray($request){
-        $return['id'] = $this->id;
-        $return['session'] = $this->session;
-        $return['subtotal'] = $this->subtotal;
-        $return['descuento'] = $this->descuento ? $this->descuento : 0 ;
-        $return['total'] = $this->total;
-        $return['items'] = SaleItemShort::collection($this->items);
-        $return['customer'] = new ContactSimple($this->cliente);
-        $return['pedido'] = $this->order_id;
-        $return['payments'] = Payment::collection($this->payments);
-        $return['created'] = new UserInExam($this->user);
-        $return['created_at'] = $this->created_at->format('Y-m-d H:i');
-        $return['updated_at'] = $this->updated_at->format('Y-m-d H:i');
+        $return = [];
+
+        if(isset($this->id)){
+            $return['id'] = $this->id;
+            $return['session'] = $this->session;
+            $return['subtotal'] = $this->subtotal;
+            $return['descuento'] = $this->descuento ? $this->descuento : 0 ;
+            $return['total'] = $this->total;
+            $return['items'] = SaleItemShort::collection($this->items);
+            $return['customer'] = new ContactSimple($this->cliente);
+            $return['pedido'] = $this->order_id;
+            $return['payments'] = Payment::collection($this->payments);
+            $return['created'] = new UserInExam($this->user);
+            $return['created_at'] = $this->created_at->format('Y-m-d H:i');
+            $return['updated_at'] = $this->updated_at->format('Y-m-d H:i'); 
+        }
+        
         return $return;
     }
 }
