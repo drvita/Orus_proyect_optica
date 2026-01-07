8192866745f3220b1b774180b969c6db8d9532c5
salvador <chavagalindo@live.com.mx>
2026-01-06 19:39:01 -0600

    task: - migrate index - hotfix helpers - improve exams

--- app/help_user.php
+++ app/help_user.php
@@ -1,16 +1,21 @@
 <?php
-function isAdmin($user)
-{
-
-    return $user->role("admin");
+if (!function_exists('isAdmin')) {
+    function isAdmin($user)
+    {
+        return $user->role("admin");
+    }
 }
 
-function user_can($user, $can)
-{
-    return $user->can($can);
+if (!function_exists('user_can')) {
+    function user_can($user, $can)
+    {
+        return $user->can($can);
+    }
 }
 
-function user_hasRole($user, $role)
-{
-    return $user->role($role);
+if (!function_exists('user_hasRole')) {
+    function user_hasRole($user, $role)
+    {
+        return $user->role($role);
+    }
 }
