8bc2a44efff8604a1aefa4527e5609a19220ad6c
salvador <chavagalindo@live.com.mx>
2026-01-17 10:20:54 -0600

    hotfix: sales order complete

--- app/Observers/ContactObserver.php
+++ app/Observers/ContactObserver.php
@@ -18,16 +18,16 @@ class ContactObserver
     public function created(Contact $contact): void
     {
         try {
-            if (!$contact->search_uuid) {
-                $request = new CreateNameRequest(
-                    name: $contact->name,
-                    id: (string) $contact->id
-                );
+            // if (!$contact->search_uuid) {
+            //     $request = new CreateNameRequest(
+            //         name: $contact->name,
+            //         id: (string) $contact->id
+            //     );
 
-                $search = $this->searchService->createName($request);
-                $contact->search_uuid = $search['id'];
-                $contact->save();
-            }
+            //     $search = $this->searchService->createName($request);
+            //     $contact->search_uuid = $search['id'];
+            //     $contact->save();
+            // }
         } catch (\Exception $e) {
             Log::error('Error al sincronizar contacto con servicio de búsqueda', [
                 'contact_id' => $contact->id,
@@ -38,39 +38,39 @@ class ContactObserver
 
     public function updated(Contact $contact)
     {
-        $type = "";
-        $dirty = $contact->getDirty();
-        unset($dirty['updated_at']);
-        unset($dirty['updated_id']);
-        $data = ["user_id" => $contact->updated_id, "inputs" => $dirty];
+        // $type = "";
+        // $dirty = $contact->getDirty();
+        // unset($dirty['updated_at']);
+        // unset($dirty['updated_id']);
+        // $data = ["user_id" => $contact->updated_id, "inputs" => $dirty];
 
-        if (is_null($contact->deleted_at)) {
-            $data['datetime'] = $contact->updated_at;
-            $type = "updated";
-        } else {
-            $data['datetime'] = $contact->deleted_at;
-            $type = "deleted";
-        }
+        // if (is_null($contact->deleted_at)) {
+        //     $data['datetime'] = $contact->updated_at;
+        //     $type = "updated";
+        // } else {
+        //     $data['datetime'] = $contact->deleted_at;
+        //     $type = "deleted";
+        // }
 
-        $contact->metas()->create(["key" => $type, "value" => $data]);
+        // $contact->metas()->create(["key" => $type, "value" => $data]);
 
-        // Sync with search service if the name was changed and search_uuid exists
-        try {
-            if (isset($dirty['name']) && $contact->search_uuid) {
-                $request = new UpdateNameRequest(
-                    point_id: $contact->search_uuid,
-                    name: $contact->name,
-                    id: (string) $contact->id
-                );
+        // // Sync with search service if the name was changed and search_uuid exists
+        // try {
+        //     if (isset($dirty['name']) && $contact->search_uuid) {
+        //         $request = new UpdateNameRequest(
+        //             point_id: $contact->search_uuid,
+        //             name: $contact->name,
+        //             id: (string) $contact->id
+        //         );
 
-                $this->searchService->updateName($request);
-            }
-        } catch (\Exception $e) {
-            Log::error('Error al actualizar contacto en servicio de búsqueda', [
-                'contact_id' => $contact->id,
-                'search_uuid' => $contact->search_uuid,
-                'error' => $e->getMessage()
-            ]);
-        }
+        //         $this->searchService->updateName($request);
+        //     }
+        // } catch (\Exception $e) {
+        //     Log::error('Error al actualizar contacto en servicio de búsqueda', [
+        //         'contact_id' => $contact->id,
+        //         'search_uuid' => $contact->search_uuid,
+        //         'error' => $e->getMessage()
+        //     ]);
+        // }
     }
 }
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\OrderSave::class => [
+            \App\Listeners\OrderSave::class,
+        ],
     ];
 
     /**
